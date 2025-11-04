09bd6ff7d6154a7fce17baed8d4e8759ce0c1ab7
kolomaxgg <kolomax@outlook.com>
2025-11-04 13:47:58 -0500

    edit profile activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.credentials)
     implementation(libs.credentials.play.services.auth)
     implementation(libs.googleid)
+    implementation(libs.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
